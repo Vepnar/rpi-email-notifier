d01412e1a5defaa3909d58d2f5982683bb1cab46
Vepnar <26712719+Vepnar@users.noreply.github.com>
2019-12-04 09:34:54 +0100

    Add usage of pytft in the main function

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 """You probably understand what this does
 
 Contributors: Arjan de Haan (Vepnar)
-Last edited: 28/10/2019 (dd/mm/yyyy)
+Last edited: 2/12/2019 (dd/mm/yyyy)
 """
 import asyncio
-from notifier import configuration, luxafor, mailbox
+from notifier import configuration, luxafor, mailbox, pitft
 
 def main():
     """ First load the modules and after that we need to startthe asynchronous loop"""
@@ -14,10 +14,12 @@ def main():
     config = configuration.get_config()
     device = luxafor.Luxafor()
     email_box = mailbox.EmailBox(device, config)
+    tft = pitft.PiTFT(config,device)
     async_loop = asyncio.get_event_loop()
 
     print('Running')
     asyncio.ensure_future(device.loop(), loop=async_loop)
+    asyncio.ensure_future(tft.gpio_loop(),loop=async_loop)
     async_loop.run_until_complete(email_box.loop())
 
 if __name__ == '__main__':
